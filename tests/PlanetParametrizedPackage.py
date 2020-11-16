777dff89f4024f9dd01f9ce212a26f5726184803
Olek <aleksander.wardyn@jit.team>
2020-11-16 17:44:58 +0100

    Write test cases for Merkury, Wenus and Mars

--- tests/PlanetParametrizedPackage.py
+++ tests/PlanetParametrizedPackage.py
@@ -8,7 +8,10 @@ class PlanetParameterizedPackage(unittest.TestCase):
         self.tmp = Planet()
 
     @parameterized.expand([
-        (1000000, 'Ziemia', 1000000)
+        (1000000, 'Ziemia', 1000000),
+        (23333311, 'Merkury', 3.07),
+        (433323, 'Wenus', 0.02),
+        (129999990, 'Mars', 2.19),
     ])
     def test_count_age_parameterized(self, earth_years, planet, expected):
         self.assertEqual(self.tmp.count_age(earth_years, planet), expected)
