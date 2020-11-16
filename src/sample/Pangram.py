93be618bfaa704b8cd1ae3045fd3333185a6cd51
Olek <aleksander.wardyn@jit.team>
2020-11-16 17:20:56 +0100

    Write code for exception

--- src/sample/Pangram.py
+++ src/sample/Pangram.py
@@ -1,6 +1,9 @@
 class Pangram:
     def is_pangram(self, word):
-        if set('abcdefghijklmnopqrstuvwxyz') - set(word.lower()) == set([]):
-            return 'True'
+        if type(word) is str:
+            if set('abcdefghijklmnopqrstuvwxyz') - set(word.lower()) == set([]):
+                return 'True'
+            else:
+                return 'False'
         else:
-            return 'False'
+            raise Exception('Must_be_a_string')
