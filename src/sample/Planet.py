682f49aee447737e536e432ac8101f988eefbd0c
Olek <aleksander.wardyn@jit.team>
2020-11-16 17:59:39 +0100

    Refactor Planet class

--- src/sample/Planet.py
+++ src/sample/Planet.py
@@ -1,26 +1,26 @@
 class Planet:
     def count_age(self, earth_years, planet):
-        if (type(earth_years) is str and type(planet) is int) or type(planet) is bool:
-            raise Exception('Zle typy')
-        if earth_years < 0:
-            raise Exception('Wiek nie moze byc ujemny')
-        stala = earth_years / 31557600
-        if planet == 'Ziemia':
-            return round(stala, 2)
-        elif planet == 'Merkury':
-            return round(stala / 0.2408467, 2)
-        elif planet == 'Wenus':
-            return round(stala / 0.61519726, 2)
-        elif planet == 'Mars':
-            return round(stala / 1.8808158, 2)
-        elif planet == 'Jowisz':
-            return round(stala / 11.862615, 2)
-        elif planet == 'Saturn':
-            return round(stala / 29.447498, 2)
-        elif planet == 'Uran':
-            return round(stala / 84.016846, 2)
-        elif planet == 'Neptun':
-            return round(stala / 164.79132, 2)
+        if type(earth_years) is int and type(planet) is str:
+            if earth_years < 0:
+                raise Exception('Wiek nie moze byc ujemny')
+            stala = earth_years / 31557600
+            if planet == 'Ziemia':
+                return round(stala, 2)
+            elif planet == 'Merkury':
+                return round(stala / 0.2408467, 2)
+            elif planet == 'Wenus':
+                return round(stala / 0.61519726, 2)
+            elif planet == 'Mars':
+                return round(stala / 1.8808158, 2)
+            elif planet == 'Jowisz':
+                return round(stala / 11.862615, 2)
+            elif planet == 'Saturn':
+                return round(stala / 29.447498, 2)
+            elif planet == 'Uran':
+                return round(stala / 84.016846, 2)
+            elif planet == 'Neptun':
+                return round(stala / 164.79132, 2)
+            else:
+                return 'Planeta nie istnieje'
         else:
-            return 'Planeta nie istnieje'
-        return ''
+            raise Exception('Zle typy')
