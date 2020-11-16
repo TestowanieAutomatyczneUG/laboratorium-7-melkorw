6e50b418265ca428a94d9ac13191a0a0becf52d3
Olek <aleksander.wardyn@jit.team>
2020-11-16 17:50:35 +0100

    Add solution for those planets

--- src/sample/Planet.py
+++ src/sample/Planet.py
@@ -9,4 +9,12 @@ class Planet:
             return round(stala / 0.61519726, 2)
         elif planet == 'Mars':
             return round(stala / 1.8808158, 2)
+        elif planet == 'Jowisz':
+            return round(stala / 11.862615, 2)
+        elif planet == 'Saturn':
+            return round(stala / 29.447498, 2)
+        elif planet == 'Uran':
+            return round(stala / 84.016846, 2)
+        elif planet == 'Neptun':
+            return round(stala / 164.79132, 2)
         return ''
