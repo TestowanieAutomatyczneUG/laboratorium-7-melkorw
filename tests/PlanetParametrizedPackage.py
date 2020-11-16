d455118a315669061b811f65f5165a574831cd2c
Olek <aleksander.wardyn@jit.team>
2020-11-16 17:43:24 +0100

    Write test for Ziemia

--- tests/PlanetParametrizedPackage.py
+++ tests/PlanetParametrizedPackage.py
@@ -7,7 +7,11 @@ class PlanetParameterizedPackage(unittest.TestCase):
     def setUp(self):
         self.tmp = Planet()
 
-
+    @parameterized.expand([
+        (1000000, 'Ziemia', 0.03)
+    ])
+    def test_count_age_parameterized(self, earth_years, planet, expected):
+        self.assertEqual(self.tmp.count_age(earth_years, planet), expected)
 
 
 if __name__ == '__main__':
