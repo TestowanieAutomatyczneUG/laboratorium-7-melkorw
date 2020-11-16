a4578b383ceccf5b65ddeb181b0467c6532c5592
Olek <aleksander.wardyn@jit.team>
2020-11-16 17:20:04 +0100

    Write exception test for pangram file

--- tests/PangramParameterizedFile.py
+++ tests/PangramParameterizedFile.py
@@ -14,3 +14,15 @@ class PangramParametrizedFile(unittest.TestCase):
                 data = line.split(" ")
                 input_value, expected = (data[0], data[1].strip("\n"))
                 self.assertEqual(temporary_pangram.is_pangram(input_value), expected)
+
+
+    def test_from_exception_file(self):
+        file = open("data/pangram_exceptions_file")
+        temporary_pangram = Pangram()
+        for line in file:
+            if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+                continue
+            else:
+                data = line.split(" ")
+                input_value, expected = (data[0], data[1].strip("\n"))
+                self.assertRaisesRegex(Exception, expected, temporary_pangram.is_pangram, int(input_value))
